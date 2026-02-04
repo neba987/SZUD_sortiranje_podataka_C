98f7ff980f5655f7077fae28a2b25bce65903a62
neba987 <nebojsaradisic2005@gmail.com>
2026-02-04 20:57:36 +0100

    Sorting array of five numbers

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5, b = 3;
+    int niz[5] = {5, 3, 4, 1, 2};
+    int i, j, temp;
 
-    if (a > b) {
-        int temp = a;
-        a = b;
-        b = temp;
+    for (i = 0; i < 5; i++) {
+        for (j = i + 1; j < 5; j++) {
+            if (niz[i] > niz[j]) {
+                temp = niz[i];
+                niz[i] = niz[j];
+                niz[j] = temp;
+            }
+        }
     }
 
-    printf("Sortirani brojevi: %d %d\n", a, b);
+    printf("Sortirani niz: ");
+    for (i = 0; i < 5; i++) {
+        printf("%d ", niz[i]);
+    }
+
     return 0;
 }
